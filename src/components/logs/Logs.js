7adb6e9d711a3e3f47682a263f85b446a0584cf1
AAYUSH KUMAR <aayushrocksall@gmail.com>
2021-06-11 11:11:20 +0530

    Add New Log

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -13,17 +13,19 @@ const Logs = ({ log: { logs, loading }, getLogs }) => {
     //eslint-disable-next-line
   }, []);
 
-  if (loading || logs.length === null) {
+  if (loading || logs === null) {
     <Preloader />;
   }
+
   return (
     <ul className='collection with-header'>
       <li className='collection-header'>
         <h4 className='center'>System Logs</h4>
       </li>
-      {!loading && logs.length === 0 ? (
+      {!loading && logs !== null && logs.length === 0 ? (
         <p className='center'>No logs available</p>
       ) : (
+        logs !== null &&
         logs.map((log) => {
           return <LogItem log={log} />;
         })
@@ -34,6 +36,7 @@ const Logs = ({ log: { logs, loading }, getLogs }) => {
 
 Logs.propTypes = {
   log: PropTypes.object.isRequired,
+  getLogs: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
